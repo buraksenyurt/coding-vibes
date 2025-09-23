e7006dce5adfdda9bf89ef0a6521543c4f458115
Burak Selim Senyurt <burakselimsenyurt@gmail.com>
2025-09-23 07:44:15 +0000

    Grok applied version 1.0.1 requests.

--- DoDi/src/DoDiApp/Pages/Index.cshtml.cs
+++ DoDi/src/DoDiApp/Pages/Index.cshtml.cs
@@ -1,19 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using DoDiApp.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace DoDiApp.Pages;
 
 public class IndexModel : PageModel
 {
-    private readonly ILogger<IndexModel> _logger;
+    private readonly ApplicationDbContext _context;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(ApplicationDbContext context)
     {
-        _logger = logger;
+        _context = context;
     }
 
-    public void OnGet()
-    {
+    public int TotalTerms { get; set; }
+    public int TotalDomains { get; set; }
+    public IList<Term> RecentTerms { get; set; } = new List<Term>();
 
+    public async Task OnGetAsync()
+    {
+        TotalTerms = await _context.Terms.CountAsync();
+        TotalDomains = await _context.Terms.Select(t => t.MainDomain).Distinct().CountAsync();
+        RecentTerms = await _context.Terms
+            .OrderByDescending(t => t.CreatedAt)
+            .Take(5)
+            .ToListAsync();
     }
 }
